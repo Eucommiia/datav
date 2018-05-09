6306482c7faeabc8cece5f36247a6fa29b9d184c
Luffy <pumeng104@163.com>
2018-05-09 15:53:10 +0800

    Datav接口API

--- src/main/java/com/juncdt/datav/DatavApplication.java
+++ src/main/java/com/juncdt/datav/DatavApplication.java
@@ -3,6 +3,12 @@ package com.juncdt.datav;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 启动类
+ *
+ * @author Lu1FFy
+ * @create 2018/05/09 15:48
+ */
 @SpringBootApplication
 public class DatavApplication {
 
