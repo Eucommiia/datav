266cc085c3268323beebb69a6788f76030fadb2b
Luffy <pumeng104@163.com>
2018-05-18 09:50:11 +0800

    Datav接口API,启动类增加跨域设置

--- src/main/java/com/juncdt/datav/DatavApplication.java
+++ src/main/java/com/juncdt/datav/DatavApplication.java
@@ -2,6 +2,11 @@ package com.juncdt.datav;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 /**
  * 启动类
@@ -10,8 +15,31 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @create 2018/05/09 15:48
  */
 @SpringBootApplication
+@EnableScheduling
 public class DatavApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DatavApplication.class, args);
 	}
+
+
+	private CorsConfiguration buildConfig() {
+		CorsConfiguration corsConfiguration = new CorsConfiguration();
+		corsConfiguration.addAllowedOrigin("*");
+		corsConfiguration.addAllowedHeader("*");
+		corsConfiguration.addAllowedMethod("*");
+		return corsConfiguration;
+	}
+
+	/**
+	 * 跨域过滤器
+	 *
+	 * @return
+	 */
+	@Bean
+	public CorsFilter corsFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", buildConfig());
+		return new CorsFilter(source);
+	}
+
 }
